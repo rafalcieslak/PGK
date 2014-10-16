a3013903cd79071929c37404c13a46eb4971e33a
Rafał Cieślak <rafalcieslak256@gmail.com>
2014-10-16 20:06:06 +0200

    Highlighting selection.

--- zad2/zad2.cpp
+++ zad2/zad2.cpp
@@ -29,6 +29,23 @@ public:
 
 GLfloat card_sizes[CARD_MODELS+1] = {6,6,6};
 
+GLfloat highlight_vertices[] = {
+	-0.5f, -0.5f,
+	0.5f, -0.5f,
+	0.5f, 0.5f,
+	-0.5f, -0.5f,
+	0.5f, 0.5f,
+	-0.5f, 0.5f,
+};
+GLfloat highlight_colors[] = {
+	1.0f, 1.0f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+};
+
 GLfloat card_vertices[CARD_MODELS+1][12] = {
 	{
 	-0.5f, -0.5f,
@@ -85,6 +102,8 @@ const unsigned int cards_no = board_height*board_width;
 const float card_width = 2.0f/board_width;
 const float card_height = 2.0f/board_height;
 
+unsigned int selection_x = 0, selection_y = 0;
+
 std::pair<float, float> card_xy_to_coords(unsigned int x, unsigned int y){
 	float xf = -1.0f + x*card_width + card_width/2.0;
 	float yf = -1.0f + y*card_height + card_height/2.0;
@@ -136,31 +155,39 @@ int main( void )
 	GLuint shader_program_id = LoadShaders( "VertexShader.vertexshader", "FragmentShader.fragmentshader" );
 
 	// Prepare uniforms of the vertex shader
-	GLint uniform_xsize, uniform_ysize, uniform_centerx, uniform_centery;
+	GLint uniform_xsize, uniform_ysize, uniform_centerx, uniform_centery, uniform_alpha;
 	uniform_xsize = glGetUniformLocation(shader_program_id,"xsize");
 	uniform_ysize = glGetUniformLocation(shader_program_id,"ysize");
 	uniform_centerx = glGetUniformLocation(shader_program_id,"center_x");
 	uniform_centery = glGetUniformLocation(shader_program_id,"center_y");
-	if(uniform_xsize == -1 || uniform_ysize == -1 || uniform_centerx == -1 || uniform_centery == -1){
+	uniform_alpha = glGetUniformLocation(shader_program_id,"alpha");
+	if(uniform_xsize == -1 || uniform_ysize == -1 || uniform_centerx == -1 || uniform_centery == -1 || uniform_alpha == -1){
 			fprintf( stderr, "A uniform is missing from shader.\n" );
 			glfwTerminate();
 			return -1;
 	}
 
-	GLuint vertexbuffer[8], colorbuffer[8];
+	// Preparing data in buffers
+	GLuint vertexbuffer[8], colorbuffer[8], hlvertices, hlcolors;
 	glGenBuffers(8, vertexbuffer);
 	glGenBuffers(8, colorbuffer);
+	glGenBuffers(1, &hlvertices);
+	glGenBuffers(1, &hlcolors);
 
 	for(int i = 0; i <= CARD_MODELS; i++){
 			glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer[i]);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * card_sizes[i] * 2, card_vertices[i], GL_STATIC_DRAW);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * card_sizes[i] * 2, card_vertices[i], GL_STATIC_DRAW);
 	}
 	for(int i = 0; i <= CARD_MODELS; i++){
 			glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[i]);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * card_sizes[i] * 3, card_colors[i], GL_STATIC_DRAW);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * card_sizes[i] * 3, card_colors[i], GL_STATIC_DRAW);
 	}
+	glBindBuffer(GL_ARRAY_BUFFER, hlvertices);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(highlight_vertices), highlight_vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, hlcolors);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(highlight_colors), highlight_colors, GL_STATIC_DRAW);
 
-	// Prepare cards
+	// Preparing cards
 	std::vector<CardState> cards;
 	for(int i = 0; i < cards_no/2; i++){
 		cards.push_back( CardState(i) );
@@ -173,60 +200,80 @@ int main( void )
 	cards[6].uncovered = true;
 	cards[8].uncovered = true;
 
+	// Enable blending
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	do{
 		// Clear the screen
 		glClear( GL_COLOR_BUFFER_BIT );
 
 		// Use our shader
 		glUseProgram(shader_program_id);
-		glUniform1f(uniform_xsize, card_width*0.9);
-		glUniform1f(uniform_ysize, card_height*0.9);
 
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
 
+		// Prepare scaling for cards
+		glUniform1f(uniform_xsize, card_width*0.9);
+		glUniform1f(uniform_ysize, card_height*0.9);
+
+		glUniform1f(uniform_alpha, 1.0f);
+
+		// Draw cards
 		for(int i = 0; i < board_height; i++){
 			for(int j = 0; j < board_width; j++){
+
+
+				// Take a card
+				const CardState& card = cards[i*board_width + j];
 
 				std::pair<float,float> pos = card_xy_to_coords(j,i);
 				glUniform1f(uniform_centerx, pos.first);
 				glUniform1f(uniform_centery, pos.second);
 
-				const CardState& card = cards[i*board_width + j];
-
 				// Determine which vertex buffer to use
-				if(card.uncovered){
-					if(card.model % 2 == 0){
-						glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer[1]);
-					}else{
-						glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer[2]);
-					}
-				}else{
-					glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer[0]);
-				}
+
+				int card_data_index;
+				if(card.uncovered)
+					card_data_index = 1 + (card.model % CARD_MODELS);
+				else
+					card_data_index = 0;
+
+				glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer[card_data_index]);
 				glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0 );
 
-				// Determine which color buffer to use
-				if(card.uncovered){
-					if(card.model % 2 == 0){
-						glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[1]);
-					}else{
-						glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[2]);
-					}
-				}else{
-					glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[0]);
-				}
+				glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[card_data_index]);
 				glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0 );
 
-
 				// Draw the card
-				glDrawArrays(GL_TRIANGLES, 0, 6);
+				glDrawArrays(GL_TRIANGLES, 0, 3*card_sizes[card_data_index]);
 
 			}
 		}
 
+		// Prepare scaling for highlight
+		glUniform1f(uniform_xsize, card_width);
+		glUniform1f(uniform_ysize, card_height);
+		// Draw highlight
+		{
+			glBindBuffer(GL_ARRAY_BUFFER, hlvertices);
+			glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0 );
+			glBindBuffer(GL_ARRAY_BUFFER, hlcolors);
+			glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0 );
+
+			std::pair<float,float> pos = card_xy_to_coords(selection_x,selection_y);
+			glUniform1f(uniform_centerx, pos.first);
+			glUniform1f(uniform_centery, pos.second);
+
+			glUniform1f(uniform_alpha, 0.35f);
+
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+		}
+
 		glDisableVertexAttribArray(1);
 		glDisableVertexAttribArray(0);
+
 
 		glUseProgram(0);
 
@@ -237,6 +284,8 @@ int main( void )
 		// Check if the ESC key was pressed or the window was closed
 		if(glfwGetKey(window, GLFW_KEY_ESCAPE ) == GLFW_PRESS || glfwWindowShouldClose(window) == 1)
 			break;
+
+
 
 	}
 	while(true);
