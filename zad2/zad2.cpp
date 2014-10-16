1541d5c776b80333263bfce25ef3c517046945e1
Rafał Cieślak <rafalcieslak256@gmail.com>
2014-10-16 21:23:45 +0200

    Card flipping animation. Added two new card models.

--- zad2/zad2.cpp
+++ zad2/zad2.cpp
@@ -1,7 +1,6 @@
 // Include standard headers
 #include <cstdio>
 #include <cstdlib>
-#include <ctime>
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -17,17 +16,21 @@ GLFWwindow* window;
 
 class CardState{
 public:
-	CardState(int m) : model(m){}
+	CardState(int m) : model(m) {}
 	int model;
 	bool uncovered = false;
 	bool removed = false;
-	mutable int flip_phase = false;
-	mutable double flip_start = 0.0;
+	mutable double animation_start = 0.0;
+	enum{
+		ANIM_MODE_UNCOVER,
+		ANIM_MODE_COVER
+	} AnimModes;
+	mutable int animation = -1;
 };
 
-#define CARD_MODELS 2
+#define CARD_MODELS 4
 
-GLfloat card_sizes[CARD_MODELS+1] = {6,6,6};
+GLfloat card_sizes[CARD_MODELS+1] = {6,6,6,6,6};
 
 GLfloat highlight_vertices[] = {
 	-0.5f, -0.5f,
@@ -48,6 +51,20 @@ GLfloat highlight_colors[] = {
 
 GLfloat card_vertices[CARD_MODELS+1][12] = {
 	{
+	-0.5f, -0.5f,
+	0.5f, -0.5f,
+	0.5f, 0.5f,
+	-0.5f, -0.5f,
+	0.5f, 0.5f,
+	-0.5f, 0.5f,
+},{
+	-0.5f, -0.5f,
+	0.5f, -0.5f,
+	0.5f, 0.5f,
+	-0.5f, -0.5f,
+	0.5f, 0.5f,
+	-0.5f, 0.5f,
+},{
 	-0.5f, -0.5f,
 	0.5f, -0.5f,
 	0.5f, 0.5f,
@@ -93,6 +110,20 @@ GLfloat card_colors[CARD_MODELS+1][18] = {
 		0.0f, 0.5f, 0.0f,
 		0.0f, 0.5f, 0.0f,
 		0.0f, 0.2f, 0.2f,
+	},{
+		0.9f, 0.6f, 0.0f,
+		1.0f, 1.0f, 0.2f,
+		0.9f, 0.6f, 0.0f,
+		0.9f, 0.6f, 0.0f,
+		0.9f, 0.6f, 0.0f,
+		1.0f, 1.0f, 0.2f,
+	},{
+		0.0f, 1.0f, 0.8f,
+		0.0f, 0.8f, 0.9f,
+		0.0f, 1.0f, 0.8f,
+		0.0f, 1.0f, 0.8f,
+		0.0f, 1.0f, 0.8f,
+		0.0f, 0.8f, 0.9f,
 	}
 };
 
@@ -103,6 +134,14 @@ const float card_width = 2.0f/board_width;
 const float card_height = 2.0f/board_height;
 
 unsigned int selection_x = 0, selection_y = 0;
+int uncovered_card = -1;
+int animated_card  = -1;
+bool uncover_animation_in_progress = false;
+const double flip_animation_length = 1.2f;
+const double animation_wait_time = 0.5f;
+
+
+std::vector<CardState> cards;
 
 std::pair<float, float> card_xy_to_coords(unsigned int x, unsigned int y){
 	float xf = -1.0f + x*card_width + card_width/2.0;
@@ -110,8 +149,44 @@ std::pair<float, float> card_xy_to_coords(unsigned int x, unsigned int y){
 	return std::make_pair(xf,yf);
 }
 
+void process_space_press(double tm){
+	int n = selection_y*board_width + selection_x;
+	if(cards[n].removed || cards[n].uncovered) return;
+	cards[n].animation_start = tm;
+	uncover_animation_in_progress = true;
+	animated_card = n;
+}
+
+void uncover_card(int n){
+	cards[n].uncovered = true;
+	uncover_animation_in_progress = false;
+	animated_card = -1;
+	if(uncovered_card == -1){
+		// this is the first card in a pair
+		uncovered_card = n;
+	}else{
+		// this is the second card
+		if( cards[n].model % CARD_MODELS == cards[uncovered_card].model % CARD_MODELS){
+			// match!
+			cards[n].removed = true;
+			cards[uncovered_card].removed = true;
+			uncovered_card = -1;
+		}else{
+			// not matching
+			cards[n].uncovered = false;
+			cards[uncovered_card].uncovered = false;
+			uncovered_card = -1;
+		}
+	}
+}
+
 int main( void )
 {
+	if( cards_no % 2 == 1){
+		std::cout << "Cards number with these dimentions is not an even number!" << std::endl;
+		return -2;
+	}
+
 	// Initialise GLFW
 	if( !glfwInit() )
 	{
@@ -155,13 +230,15 @@ int main( void )
 	GLuint shader_program_id = LoadShaders( "VertexShader.vertexshader", "FragmentShader.fragmentshader" );
 
 	// Prepare uniforms of the vertex shader
-	GLint uniform_xsize, uniform_ysize, uniform_centerx, uniform_centery, uniform_alpha;
+	GLint uniform_xsize, uniform_ysize, uniform_centerx, uniform_centery, uniform_alpha, uniform_animphase, uniform_reversed;
 	uniform_xsize = glGetUniformLocation(shader_program_id,"xsize");
 	uniform_ysize = glGetUniformLocation(shader_program_id,"ysize");
 	uniform_centerx = glGetUniformLocation(shader_program_id,"center_x");
 	uniform_centery = glGetUniformLocation(shader_program_id,"center_y");
 	uniform_alpha = glGetUniformLocation(shader_program_id,"alpha");
-	if(uniform_xsize == -1 || uniform_ysize == -1 || uniform_centerx == -1 || uniform_centery == -1 || uniform_alpha == -1){
+	uniform_animphase = glGetUniformLocation(shader_program_id,"animation_phase");
+	uniform_reversed = glGetUniformLocation(shader_program_id,"reversed");
+	if(uniform_xsize == -1 || uniform_ysize == -1 || uniform_centerx == -1 || uniform_centery == -1 || uniform_alpha == -1 || uniform_animphase == -1 || uniform_reversed == -1){
 			fprintf( stderr, "A uniform is missing from shader.\n" );
 			glfwTerminate();
 			return -1;
@@ -188,25 +265,22 @@ int main( void )
 	glBufferData(GL_ARRAY_BUFFER, sizeof(highlight_colors), highlight_colors, GL_STATIC_DRAW);
 
 	// Preparing cards
-	std::vector<CardState> cards;
 	for(int i = 0; i < cards_no/2; i++){
 		cards.push_back( CardState(i) );
 		cards.push_back( CardState(i) );
 	}
-
     std::random_shuffle(cards.begin(), cards.end());
-	cards[3].uncovered = true;
-	cards[4].uncovered = true;
-	cards[6].uncovered = true;
-	cards[8].uncovered = true;
 
 	// Enable blending
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	// Prepare input tresholds
 	bool key_pressed_up = false, key_pressed_down = false, key_pressed_left = false, key_pressed_right = false, key_pressed_space = false;
 
 	do{
+		double tm = glfwGetTime();
+
 		// Clear the screen
 		glClear( GL_COLOR_BUFFER_BIT );
 
@@ -215,44 +289,6 @@ int main( void )
 
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
-
-		// Prepare scaling for cards
-		glUniform1f(uniform_xsize, card_width*0.9);
-		glUniform1f(uniform_ysize, card_height*0.9);
-
-		glUniform1f(uniform_alpha, 1.0f);
-
-		// Draw cards
-		for(int i = 0; i < board_height; i++){
-			for(int j = 0; j < board_width; j++){
-
-
-				// Take a card
-				const CardState& card = cards[i*board_width + j];
-
-				std::pair<float,float> pos = card_xy_to_coords(j,i);
-				glUniform1f(uniform_centerx, pos.first);
-				glUniform1f(uniform_centery, pos.second);
-
-				// Determine which vertex buffer to use
-
-				int card_data_index;
-				if(card.uncovered)
-					card_data_index = 1 + (card.model % CARD_MODELS);
-				else
-					card_data_index = 0;
-
-				glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer[card_data_index]);
-				glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0 );
-
-				glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[card_data_index]);
-				glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0 );
-
-				// Draw the card
-				glDrawArrays(GL_TRIANGLES, 0, 3*card_sizes[card_data_index]);
-
-			}
-		}
 
 		// Prepare scaling for highlight
 		glUniform1f(uniform_xsize, card_width);
@@ -268,9 +304,55 @@ int main( void )
 			glUniform1f(uniform_centerx, pos.first);
 			glUniform1f(uniform_centery, pos.second);
 
-			glUniform1f(uniform_alpha, 0.35f);
+			glUniform1f(uniform_alpha, 1.0f);
 
 			glDrawArrays(GL_TRIANGLES, 0, 6);
+		}
+
+		// Prepare scaling for cards
+		glUniform1f(uniform_xsize, card_width*0.9);
+		glUniform1f(uniform_ysize, card_height*0.9);
+
+		glUniform1f(uniform_alpha, 1.0f);
+
+		// Draw cards
+		for(int i = 0; i < board_height; i++){
+			for(int j = 0; j < board_width; j++){
+
+				// Take a card
+				int n = i*board_width + j;
+
+				std::pair<float,float> pos = card_xy_to_coords(j,i);
+				glUniform1f(uniform_centerx, pos.first);
+				glUniform1f(uniform_centery, pos.second);
+
+				float animation_phase;
+				if(animated_card == n) animation_phase = tm - cards[n].animation_start;
+				else animation_phase = 0.0;
+				glUniform1f(uniform_animphase, animation_phase);
+
+				if(animation_phase >= 1.0){
+					animation_phase = 1.0;
+					uncover_card(n);
+				}
+
+				// Determine which vertex buffer to use
+				int card_data_index;
+				if(cards[n].uncovered || animation_phase >= 0.5) card_data_index = 1 + (cards[n].model % CARD_MODELS);
+				else card_data_index = 0;
+
+				glUniform1i(uniform_reversed,cards[n].uncovered);
+
+				glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer[card_data_index]);
+				glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0 );
+
+				glBindBuffer(GL_ARRAY_BUFFER, colorbuffer[card_data_index]);
+				glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0 );
+
+				// Draw the card
+				glDrawArrays(GL_TRIANGLES, 0, 3*card_sizes[card_data_index]);
+
+			}
 		}
 
 		glDisableVertexAttribArray(1);
@@ -288,14 +370,19 @@ int main( void )
 			break;
 
 		// Selection movement keys
-		if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_RELEASE)    key_pressed_up = false;
+		if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_RELEASE)    key_pressed_up =  false;
 		if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS &&    key_pressed_up == false    && selection_y < board_height - 1) { selection_y++; key_pressed_up = true;}
-		if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_RELEASE)  key_pressed_down = false;
+		if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_RELEASE)  key_pressed_down =  false;
 		if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS &&  key_pressed_down == false  && selection_y > 0)                { selection_y--; key_pressed_down = true;}
-		if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_RELEASE)  key_pressed_left = false;
+		if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_RELEASE)  key_pressed_left =  false;
 		if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS &&  key_pressed_left == false  && selection_x > 0)                { selection_x--; key_pressed_left = true;}
-		if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_RELEASE) key_pressed_right = false;
+		if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_RELEASE) key_pressed_right =  false;
 		if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS && key_pressed_right == false && selection_x < board_width - 1)  { selection_x++; key_pressed_right = true;}
+		if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE) key_pressed_space =  false;
+		if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && key_pressed_space == false && uncover_animation_in_progress == false){
+			key_pressed_right = true;
+			process_space_press(tm);
+		}
 
 	}
 	while(true);
