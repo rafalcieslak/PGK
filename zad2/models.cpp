de620a970fbff52e5319bffc652a38a1d1032657
Rafał Cieślak <rafalcieslak256@gmail.com>
2014-10-17 20:53:36 +0200

    Improved code formatting and layout

--- zad2/models.cpp
+++ zad2/models.cpp
@@ -24,71 +24,71 @@ GLfloat card_vertices[CARD_MODELS+2][48] = {
 		-0.5f, -0.5f, 0.5f, -0.5f,  0.5f, 0.5f,
 		-0.5f, -0.5f, 0.5f,  0.5f, -0.5f, 0.5f,
 	},{ // fat line
-		-1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,
-		-1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,
-		-0.5f, 0.75f, 0.75f, -0.5f, 0.5f, -0.75f,
-		-0.5f, 0.75f, -0.75f, 0.5f, 0.5f, -0.75f,
+		-1.0f, -1.0f,   1.0f,  -1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,   1.0f,   1.0f, -1.0f,  1.0f,
+		-0.5f,  0.75f,  0.75f, -0.5f,  0.5f, -0.75f,
+		-0.5f,  0.75f, -0.75f,  0.5f,  0.5f, -0.75f,
 	},{ // cross
-		-1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,
-		-1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,
-		-0.5f, -0.25f, 0.5f, -0.25f, 0.5f, 0.25f,
-		-0.5f, -0.25f, -0.5f, 0.25f, 0.5f, 0.25f,
-		-0.25f, -0.5f, 0.25f, -0.5f, 0.25f, 0.5f,
-		-0.25f, -0.5f, -0.25f, 0.5f, 0.25f, 0.5f,
+		-1.0f,  -1.0f,   1.0f,  -1.0f,  1.0f,  1.0f,
+		-1.0f,  -1.0f,   1.0f,   1.0f, -1.0f,  1.0f,
+		-0.5f,  -0.25f,  0.5f,  -0.25f, 0.5f,  0.25f,
+		-0.5f,  -0.25f, -0.5f,   0.25f, 0.5f,  0.25f,
+		-0.25f, -0.5f,   0.25f, -0.5f,  0.25f, 0.5f,
+		-0.25f, -0.5f,  -0.25f,  0.5f,  0.25f, 0.5f,
 	},{ // five-pointed star
-		-1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,
-		-1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,
-		0.362f, -0.498f, 0.132f, 0.190f, -0.223f, -0.072f,
-		0.585f, 0.190f, 0.0f, -0.235f, -0.585f, 0.190f,
-		0.0f, 0.615f, 0.223f, -0.072f, -0.362f, -0.498f,
+		-1.0f,   -1.0f,   1.0f,   -1.0f,    1.0f,    1.0f,
+		-1.0f,   -1.0f,   1.0f,    1.0f,   -1.0f,    1.0f,
+		 0.362f, -0.498f, 0.132f,  0.190f, -0.223f, -0.072f,
+		 0.585f,  0.190f, 0.0f,   -0.235f, -0.585f,  0.190f,
+		 0.0f,    0.615f, 0.223f, -0.072f, -0.362f, -0.498f,
 	},{ // hexagon
-		-1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,
-		-1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,
-		-0.283f, 0.490f, 0.283f, 0.490f, -0.566f, 0.0f,
-		0.283f, 0.490f, 0.566f, 0.0f, -0.566f, 0.0f,
+		-1.0f,   -1.0f,   1.0f,   -1.0f,    1.0f,   1.0f,
+		-1.0f,   -1.0f,   1.0f,    1.0f,   -1.0f,   1.0f,
+		-0.283f,  0.490f, 0.283f,  0.490f, -0.566f, 0.0f,
+		 0.283f,  0.490f, 0.566f,  0.0f,   -0.566f, 0.0f,
 		-0.283f, -0.490f, 0.283f, -0.490f, -0.566f, 0.0f,
-		0.283f, -0.490f, 0.566f, 0.0f, -0.566f, 0.0f,
+		 0.283f, -0.490f, 0.566f,  0.0f,   -0.566f, 0.0f,
 	},{ // rhombus
-		-1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,
-		-1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,
-		0.75f, -0.5f, -0.25f, -0.5f, 0.25f, 0.5f,
-		-0.25f, -0.5f, 0.25f, 0.5f, -0.75f, 0.5f,
+		-1.0f,  -1.0f,  1.0f,  -1.0f,  1.0f,  1.0f,
+		-1.0f,  -1.0f,  1.0f,   1.0f, -1.0f,  1.0f,
+		 0.75f, -0.5f, -0.25f, -0.5f,  0.25f, 0.5f,
+		-0.25f, -0.5f,  0.25f,  0.5f, -0.75f, 0.5f,
 	},{ // two triangles
-		-1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,
-		-1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,
+		-1.0f,  -1.0f,  1.0f, -1.0f,  1.0f,  1.0f,
+		-1.0f,  -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,
 		-0.25f, -0.5f, -0.25f, 0.5f, -0.75f, 0.0f,
-		0.25f, -0.5f, 0.25f, 0.5f, 0.75f, 0.0f,
+		 0.25f, -0.5f,  0.25f, 0.5f,  0.75f, 0.0f,
 	}
 };
 
 #define RGB(r,g,b) r/255.0f, g/255.0f, b/255.0f
 
-#define COLOR_D_BROWN RGB(85, 46, 1)
-#define COLOR_L_BROWN RGB(150, 81, 2)
-#define COLOR_M_BLUE RGB(8, 117, 241)
-#define COLOR_D_BLUE RGB(25, 77, 137)
-#define COLOR_H_BLUE RGB(5, 36, 71)
-#define COLOR_M_GREEN RGB(35, 184, 82)
-#define COLOR_D_GREEN RGB(38, 94, 56)
-#define COLOR_H_GREEN RGB(0, 66, 20)
+#define COLOR_D_BROWN  RGB(85, 46, 1)
+#define COLOR_L_BROWN  RGB(150, 81, 2)
+#define COLOR_M_BLUE   RGB(8, 117, 241)
+#define COLOR_D_BLUE   RGB(25, 77, 137)
+#define COLOR_H_BLUE   RGB(5, 36, 71)
+#define COLOR_M_GREEN  RGB(35, 184, 82)
+#define COLOR_D_GREEN  RGB(38, 94, 56)
+#define COLOR_H_GREEN  RGB(0, 66, 20)
 #define COLOR_M_YELLOW RGB(234, 234, 14)
 #define COLOR_D_YELLOW RGB(209, 209, 89)
 #define COLOR_H_YELLOW RGB(168, 168, 0)
-#define COLOR_M_RED RGB(226, 65, 65)
-#define COLOR_D_RED RGB(96, 25, 25)
-#define COLOR_H_RED RGB(178, 0, 0)
+#define COLOR_M_RED    RGB(226, 65, 65)
+#define COLOR_D_RED    RGB(96, 25, 25)
+#define COLOR_H_RED    RGB(178, 0, 0)
 #define COLOR_M_VIOLET RGB(146, 42, 178)
 #define COLOR_D_VIOLET RGB(98, 47, 114)
 #define COLOR_H_VIOLET RGB(76, 0, 99)
-#define COLOR_M_GRAY RGB(165, 165, 165)
-#define COLOR_D_GRAY RGB(117, 117, 117)
-#define COLOR_H_GRAY RGB(66, 66, 66)
-#define COLOR_M_PINK RGB(209, 43, 148)
-#define COLOR_D_PINK RGB(117, 49, 92)
-#define COLOR_H_PINK RGB(153, 0, 96)
-#define COLOR_M_CYAN RGB(58, 175, 160)
-#define COLOR_D_CYAN RGB(42, 94, 87)
-#define COLOR_H_CYAN RGB(0, 91, 79)
+#define COLOR_M_GRAY   RGB(165, 165, 165)
+#define COLOR_D_GRAY   RGB(117, 117, 117)
+#define COLOR_H_GRAY   RGB(66, 66, 66)
+#define COLOR_M_PINK   RGB(209, 43, 148)
+#define COLOR_D_PINK   RGB(117, 49, 92)
+#define COLOR_H_PINK   RGB(153, 0, 96)
+#define COLOR_M_CYAN   RGB(58, 175, 160)
+#define COLOR_D_CYAN   RGB(42, 94, 87)
+#define COLOR_H_CYAN   RGB(0, 91, 79)
 
 GLfloat card_colors[CARD_MODELS+1][60] = {
 	{
@@ -145,9 +145,9 @@ GLfloat card_colors[CARD_MODELS+1][60] = {
 
 GLfloat highlight_vertices[] = {
 	-1.0f, -1.0f, -1.0f, -0.5f, -0.5f, -1.0f,
-	1.0f, -1.0f, 1.0f, -0.5f, 0.5f, -1.0f,
-	-1.0f, 1.0f, -1.0f, 0.5f, -0.5f, 1.0f,
-	1.0f, 1.0f, 1.0f, 0.5f, 0.5f, 1.0f,
+	 1.0f, -1.0f,  1.0f, -0.5f,  0.5f, -1.0f,
+	-1.0f,  1.0f, -1.0f,  0.5f, -0.5f,  1.0f,
+	 1.0f,  1.0f,  1.0f,  0.5f,  0.5f,  1.0f,
 };
 GLfloat highlight_colors[] = {
 	1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f,
